5362b8e3b0ffbc7b306ac323c864d73278616e25
Ashik Al Habib <habib62@uwindsor.ca>
2024-07-27 22:07:28 -0400

    - Adjusted Screen - Added "Screen Close" Function

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ import pygame
 pygame.init()
 
 #create game windsow
-SCREEN_WIDTH = 100
-SCREEN_HEIGHT = 800
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Action Game")
 
+#game window loop
+run = True
+
+while run:
+    #event handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
