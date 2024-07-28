0337cf5621ab407f8c94e15a31758e39d9f8ae19
Ashik Al Habib <habib62@uwindsor.ca>
2024-07-28 17:40:40 -0400

    - Added countdown timer to start fighting

--- main.py
+++ main.py
@@ -19,6 +19,10 @@ RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
 
+#define game variables
+intro_count = 3
+last_count_update = pygame.time.get_ticks()
+
 #define fighter variables
 WARRIOR_SIZE = 162
 WARRIOR_SCALE = 4
@@ -40,6 +44,16 @@ wizard_sheet = pygame.image.load("assets/images/wizard/Sprites/wizard.png").conv
 WARRIOR_ANIMATION_STEPS = [10, 8, 1, 7, 7, 3, 7]
 WIZARD_ANIMATION_STEPS = [8, 8, 1, 8, 8, 3, 7]
 
+#define fonts
+count_font = pygame.font.Font("assets/fonts/turok.ttf", 100)
+score_font = pygame.font.Font("assets/fonts/turok.ttf", 30)
+
+#function for drawing text
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
+
+
 #function for loading bg
 def load_bg():
     scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -53,8 +67,8 @@ def draw_health_bar(health, x, y):
     pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
     
 #create two fighters
-figther_1 = Fighter(200, 310, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS)
-figther_2 = Fighter(700, 310, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_STEPS)
+figther_1 = Fighter(1, 200, 310, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS)
+figther_2 = Fighter(2, 700, 310, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_STEPS)
 
 #game window loop
 run = True
@@ -70,9 +84,18 @@ while run:
     draw_health_bar(figther_1.health, 20, 20)
     draw_health_bar(figther_2.health, 580, 20)
     
-    #move fighters
-    figther_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, figther_2)
-    # figther_2.move(SCREEN_WIDTH, SCREEN_HEIGHT)
+    #update countdown timer
+    if intro_count <= 0:
+        #move fighters
+        figther_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, figther_2)
+        figther_2.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, figther_1)
+    else:
+        #display count timer
+        draw_text(str(intro_count), count_font, YELLOW, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        #update count timer
+        if (pygame.time.get_ticks() - last_count_update) >= 1000:
+            intro_count -= 1
+            last_count_update = pygame.time.get_ticks()
     
     #update fighters
     figther_1.update()
