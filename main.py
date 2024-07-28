73098f57f374154e36e0f05ed40817662536df8f
Ashik Al Habib <habib62@uwindsor.ca>
2024-07-27 22:19:06 -0400

    - Added and Adjusted a Background Screen

--- main.py
+++ main.py
@@ -9,11 +9,27 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Action Game")
 
+#load bg image
+bg_image = pygame.image.load("assets/images/background/background.jpg").convert_alpha()
+
+#function for loading bg
+def load_bg():
+    scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.blit(scaled_bg, (0, 0))
+
 #game window loop
 run = True
 
 while run:
+    #load bg
+    load_bg()
+    
     #event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    #update display
+    pygame.display.update()
+    
+#exit pygame
+pygame.QUIT()
